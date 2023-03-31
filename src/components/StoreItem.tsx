27127a826c9b5c6556a8a3a33290258d54e4d1f0
Robin Simonot <simonot.robin@gmail.com>
2023-03-31 13:51:40 +0200

    Started the Store part

---
+++ src/components/StoreItem.tsx
@@ -0,0 +1,35 @@
+import { Card } from "react-bootstrap";
+
+type StoreItemProps = {
+  id: number;
+  name: string;
+  price: number;
+  description: string;
+  imgUrl: string;
+};
+
+export function StoreItem({
+  id,
+  name,
+  price,
+  description,
+  imgUrl,
+}: StoreItemProps) {
+  return (
+    <Card style={{ "flex-direction": "row" }}>
+      <Card.Img
+        variant="left"
+        src={imgUrl}
+        width="50%"
+        // style={{ objectFit: "cover" }}
+      />
+      <Card.Body className="d-flex flex-column">
+        <Card.Title className="d-flex justify-content-between align-items-baseline mb-4">
+          <span className="fs-2">{name}</span>
+          <span className="ms-2 text-muted">{price}</span>
+        </Card.Title>
+        <Card.Text>{description}</Card.Text>
+      </Card.Body>
+    </Card>
+  );
+}
