c2c6f827c0811574a1615f2d50ef13584c1a8c71
Robin Simonot <simonot.robin@gmail.com>
2023-04-04 13:56:05 +0200

    Added quantity management, no style

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,5 +1,6 @@
-import { Card } from "react-bootstrap";
+import { Button, Card, Col } from "react-bootstrap";
 import { formatCurrency } from "../utilities/formatCurrency";
+import AddToCartImage from "../images/add-to-cart.svg";
 
 type StoreItemProps = {
   id: number;
@@ -16,12 +17,13 @@ export function StoreItem({
   description,
   imgUrl,
 }: StoreItemProps) {
+  const quantity = 0;
   return (
-    <Card style={{ "flex-direction": "row" }}>
+    <Card className="h-100" style={{ "flex-direction": "row" }}>
       <Card.Img
         variant="left"
         src={imgUrl}
-        width="50%"
+        width="30%"
         // style={{ objectFit: "cover" }}
       />
       <Card.Body className="d-flex flex-column">
@@ -29,7 +31,31 @@ export function StoreItem({
           <span className="fs-3">{name}</span>
           <span className="ms-2 text-muted">{formatCurrency(price)}</span>
         </Card.Title>
-        <Card.Text>{description}</Card.Text>
+        <Card.Text className="d-flex justify-content-between">
+          <Col className="col-10">{description}</Col>
+          <Col>
+            <div className="mt-auto">
+              {quantity === 0 ? (
+                <Button style={{ height: "3rem", width: "3rem" }}>
+                  <img src={AddToCartImage} alt="ADD_TO_CART"></img>
+                </Button>
+              ) : (
+                <div
+                  className="d-flex align-items-center flex-column"
+                  style={{ gap: ".5rem" }}
+                >
+                  <div
+                    className="d-flex align-items-center justify-content-center"
+                    style={{ gap: ".5rem" }}
+                  >
+                    +/-
+                  </div>
+                  Remove
+                </div>
+              )}
+            </div>
+          </Col>
+        </Card.Text>
       </Card.Body>
     </Card>
   );
